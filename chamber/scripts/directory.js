b1fbd45cb94191ccd5458619d2286370114182c9
velossoraptor <taliamolsen@gmail.com>
2025-05-15 10:00:24 -0400

    Grid and nav starting to work

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,19 +1,42 @@
 const currentyear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 const today = new Date();
-const hamButton = document.querySelector('#hamburger');
-const navigation = document.querySelector('nav');
-const activeLink = document.querySelector('.active');
-const memberCards = document.querySelector('.member-cards');
 
-activeLink.innerHTML = `🦕${activeLink.innerHTML}`;
+const hamButton = document.querySelector('#hamburger');
+const listButton = document.querySelector('#list-toggle');
+
+const navigation = document.querySelector('#mobile-nav');
+const activeLink = document.querySelectorAll('.active');
+
+const memberCards = document.querySelector('.member-cards');
+const cardList = document.querySelectorAll('.card');
+
+// activeLink.innerHTML = `🦕${activeLink.innerHTML}`;
+activeLink.forEach(link => {
+    link.innerHTML = `🦕${link.innerHTML}`;
+    link.setAttribute('color', 'black');
+});
 
 currentyear.innerHTML = today.getFullYear();
 lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+listButton.addEventListener('click', () => {
+    memberCards.classList.toggle('list');
+    listButton.classList.toggle('list-button');
+    cardList.forEach(card => {
+        card.classList.toggle('list-card');
+    });
+    let hasClass = listButton.classList.contains('list-button');
+    if (hasClass) {
+        listButton.textContent = `Photo Grid`;
+    } else {
+        listButton.textContent = `Simple List`;
+    }
 });
 
 async function getMemberData() {
@@ -25,21 +48,21 @@ async function getMemberData() {
 getMemberData();
 
 const displayMembers = (members) => {
-    members.forEach((member) =>{
+    members.forEach((member) => {
         let card = document.createElement('section');
-		let nameDiv = document.createElement('div');
-		let contentDiv = document.createElement('div');
-		let textDiv = document.createElement('div');
+        let nameDiv = document.createElement('div');
+        let contentDiv = document.createElement('div');
+        let textDiv = document.createElement('div');
         let name = document.createElement('h2');
         let address = document.createElement('p');
-		let logo = document.createElement('img');
+        let logo = document.createElement('img');
         let email = document.createElement('p');
         let phone = document.createElement('p');
-		let url = document.createElement('a');
+        let url = document.createElement('a');
 
-		nameDiv.setAttribute('id', 'card-header');
-		contentDiv.setAttribute('id', 'card-content');
-		textDiv.setAttribute('id', 'card-text');
+        nameDiv.setAttribute('id', 'card-header');
+        contentDiv.setAttribute('id', 'card-content');
+        textDiv.setAttribute('id', 'card-text');
 
         name.textContent = `${member.name}`;
 
@@ -50,26 +73,28 @@ const displayMembers = (members) => {
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '100');
 
-		email.innerHTML = `<b>Email:</b> ${member.email}`;
-		phone.innerHTML = `<b>Phone:</b>${member.phone}`;
+        email.innerHTML = `<b>Email:</b> ${member.email}`;
+        phone.innerHTML = `<b>Phone:</b>${member.phone}`;
 
-		url.setAttribute('href', `${member.url}`);
-		url.innerHTML = `<b>Link:</b> ${member.name}`;
+        url.setAttribute('href', `${member.url}`);
+        url.innerHTML = `<b>Link:</b> ${member.name}`;
 
         nameDiv.appendChild(name);
         nameDiv.appendChild(address);
 
-		contentDiv.appendChild(logo);
+        contentDiv.appendChild(logo);
         textDiv.appendChild(email);
-		textDiv.appendChild(phone);
-		textDiv.appendChild(url);
-	
-		contentDiv.appendChild(textDiv);
+        textDiv.appendChild(phone);
+        textDiv.appendChild(url);
 
-		card.appendChild(nameDiv);
-		card.appendChild(contentDiv);
-        
-		
+        contentDiv.appendChild(textDiv);
+
+        card.appendChild(nameDiv);
+        card.appendChild(contentDiv);
+
+        card.setAttribute('class', 'card');
+
+
 
         memberCards.appendChild(card);
     });

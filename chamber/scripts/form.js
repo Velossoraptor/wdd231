e105ad682bf7c8729d9dc52dc0e2d7c143dd8eec
velossoraptor <taliamolsen@gmail.com>
2025-05-27 14:15:28 -0400

    First test of Thank you page

---
+++ chamber/scripts/form.js
@@ -0,0 +1,30 @@
+const timestamp = document.querySelector("#timestamp");
+const fullName = document.querySelector("#name");
+const organization = document.querySelector("#organization");
+const date = document.querySelector("#date");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+
+const params = new URLSearchParams(window.location.search);
+const fname = `${params.get('fname')} ${params.get('lname')}`;
+const orgName = `${params.get('organization')}`;
+const subDate = `${params.get('date')}`;
+const subEmail = `${params.get('email')}`;
+const subPhone = `${params.get('phone')}`;
+
+if(timestamp != null){
+    timestamp.innerHTML = Date();
+}
+
+
+function personalThankYou(){
+    fullName.innerHTML = fname;
+    organization.innerHTML = orgName;
+    date.innerHTML = subDate;
+    email.innerHTML = subEmail;
+    phone.innerHTML = subPhone;
+}
+
+if(params != `null` && fname != `null null`){
+    personalThankYou();
+}

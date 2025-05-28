d2a3fcddbf57b8793ca61f27dd87040c2b886812
velossoraptor <taliamolsen@gmail.com>
2025-05-28 09:28:16 -0400

    Modals added

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -12,6 +12,15 @@ const subDate = `${params.get('timestamp')}`;
 const subEmail = `${params.get('email')}`;
 const subPhone = `${params.get('phone')}`;
 
+const npModal = document.querySelector("#np");
+const brModal = document.querySelector("#br");
+const siModal = document.querySelector("#si");
+const goModal = document.querySelector("#go");
+const buttonNp = document.querySelector("#learn-more-np");
+const buttonBr = document.querySelector("#learn-more-br");
+const buttonSi = document.querySelector("#learn-more-si");
+const buttonGo = document.querySelector("#learn-more-go");
+
 if(timestamp != null){
     timestamp.value = Date();
     console.log(timestamp);
@@ -29,3 +38,36 @@ function personalThankYou(){
 if(params != `null` && fname != `null null`){
     personalThankYou();
 }
+
+buttonNp.addEventListener("click", () =>{
+    npModal.showModal();
+    npModal.addEventListener("click", (event) => {
+        if(event.target==npModal){
+            npModal.close();
+        }
+    });
+});
+buttonBr.addEventListener("click", () =>{
+    brModal.showModal();
+    brModal.addEventListener("click", (event) => {
+        if(event.target==brModal){
+            brModal.close();
+        }
+    });
+});
+buttonSi.addEventListener("click", () =>{
+    siModal.showModal();
+    siModal.addEventListener("click", (event) => {
+        if(event.target==siModal){
+            siModal.close();
+        }
+    });
+});
+buttonGo.addEventListener("click", () =>{
+    goModal.showModal();
+    goModal.addEventListener("click", (event) => {
+        if(event.target==goModal){
+            goModal.close();
+        }
+    });
+});
